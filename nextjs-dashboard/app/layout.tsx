891d1d05636cf8acde1d3a4c577f485f1f302060
Jevrec <tobigerm7@gmail.com>
2025-09-16 09:38:02 +0200

    Chapter 4
    Creating Layouts and Pages

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-
+ 
 export default function RootLayout({
   children,
 }: {
@@ -11,4 +11,4 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
-  }
+}
